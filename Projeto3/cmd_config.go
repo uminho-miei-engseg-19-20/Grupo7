7888036dc5b76bd1bdd59323c5d97a5c020a2b95
robertocachada <36969807+robertocachada@users.noreply.github.com>
2020-07-06 22:23:18 +0100

    cmd_config

---
+++ Projeto3/cmd_config.go
@@ -0,0 +1,8 @@
+package auxi
+
+// Alterar para a aplicationID fornecida pela AMA
+var applicationID string = "b826359c-06f8-425e-8ec3-50a97a418916"
+
+func getAppid() string{
+	return applicationID
+}
